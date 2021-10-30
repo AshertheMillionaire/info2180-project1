715d82141a68612cdae7dbc760d619bd8073883d
Maurice M. Marshalleck <maurice.marshalleck@jcdc.local>
2021-10-30 17:49:49 -0500

    added index.html

--- info2180-project1-starter-master/README.md
+++ info2180-project1-starter-master/README.md
@@ -1,3 +1,2 @@
 # INFO2180 Project 1
-
-This is Project 1 for <Your Name>
+This is Project 1 for Donique Forbes
--- info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/scripts.js
@@ -1,1 +1,4 @@
 /* Add your JavaScript to this file */
+function subscription()
+
+var form = document.getElementbyclass()
